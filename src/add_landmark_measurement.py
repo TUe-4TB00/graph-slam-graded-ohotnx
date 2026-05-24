ba5c6dfec870991ebe529086a5010572597ef518
Max Popov <m.popov@student.tue.nl>
2026-05-24 18:56:24 +0200

    Done up to exercise 9

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -9,7 +9,11 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_landmark_measurement(graph, initial_estimate, result):
     # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
-    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
+    relVec = np.array([4 - 5.41421356, 2 - 1.41421356])
+    dirVec = np.array([0, 1])
+
+    rotation = np.arccos(np.dot(relVec, dirVec)/(np.linalg.norm(relVec)*np.linalg.norm(dirVec)))
+    distance = np.linalg.norm(relVec)
+    print(rotation, np.degrees(rotation))
+    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(np.degrees(rotation)), distance, MEASUREMENT_NOISE))
     return graph
--- src/add_pose.py
+++ src/add_pose.py
@@ -9,8 +9,14 @@ ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # 
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
 def add_pose(graph, initial_estimate):
-    # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
+    # TODO: Add the odometry factor between X(3) and X(4) to the graph (BetweenFactorPose2)
+    # Between X(3) and X(4): Rotate 45 deg, move forward 2m, rotate 45 deg
+    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), gtsam.Pose2(np.sqrt(2), np.sqrt(2), np.pi/2), ODOMETRY_NOISE))
 
-    # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
+    # TODO: Based on the odometry, find the initial estimate for the pose of X(4) and add it to the graph
+    initial_estimate.insert(X(4), gtsam.Pose2(4 + np.sqrt(2), np.sqrt(2), np.pi/2))
+
+    # Print the initial estimate
+    print("Initial Estimate:\n{}".format(initial_estimate))
     
     return graph, initial_estimate
